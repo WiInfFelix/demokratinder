41bd881fff2a115e6f0a3967bc5de0e99fce0bdb
Mats <mats.scharnberg@study.hs-duesseldorf.de>
2019-09-29 13:59:31 +0200

    Cookie wird bei Erstaufruf der Seite hinzugefügt bzw abgeglichen

--- app.py
+++ app.py
@@ -1,16 +1,34 @@
-from bottle import route, run, template
+from bottle import route, run, template, request, response, get
 from enum import Enum
+import socket
 
 class VoteEnum(Enum):
     LIKE = 1
     NOPE = 0
+    NEUTRAL = 2
 
 
-votedict = {}
+voteDict = {}
 
-@route('/hello/<name>')
-def index(name):
-    return template('<b>Hello {{name}}, du neidischer Schwanz! Du Fanboi!</b>!', name=name)
+cookieName = "DemokratinerID"
+
+hostname = socket.gethostname()
+IPAddr = socket.gethostbyname(hostname)
 
 
-run(host='192.168.178.183', port=8080, debug=True)
+@get('/')
+def index():
+    if request.get_cookie(cookieName):
+        clientID = request.get_cookie(cookieName)
+        print(voteDict)
+        if clientID in voteDict:
+            return template('<b>Hello {{name}}, du neidischer Schwanz! Du Fanboi! Schön, dass du wieder hier bist</b>!', name=clientID)
+    else:
+        countTinderer = str(len(voteDict) + 1)
+        clientID = "TinderGuru " + countTinderer
+        response.set_cookie(cookieName, clientID)
+        voteDict[clientID] = VoteEnum.NEUTRAL
+        print(voteDict)
+        return template('<b>Hello {{name}}, du neidischer Schwanz! Du Fanboi! Du bist jetzt dabei!</b>!', name=clientID)
+
+run(host=IPAddr, port=8080, debug=True)
