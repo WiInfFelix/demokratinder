37d976c0de5b77602f68a7dff80b950fd5b165f0
Mats <mats.scharnberg@study.hs-duesseldorf.de>
2019-10-06 18:55:29 +0200

    Cookiesabfrage angepasst

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ hostname = socket.gethostname()
 IPAddr = socket.gethostbyname(hostname)
 current_dir = os.path.dirname(os.path.realpath(__file__))
 viewfolder = os.path.join(current_dir, 'views\\')
-os.remove(viewfolder + "qrcode.png")
-code = qrcode.make("https://" + IPAddr + ":8080")
+#os.remove(viewfolder + "qrcode.png")
+code = qrcode.make("http://" + IPAddr + ":8080")
 code.save(viewfolder + "qrcode.png", 'PNG')
 
 @get('/')
@@ -28,22 +28,24 @@ def index():
     try:
         if request.get_cookie(cookieName):
             clientID = request.get_cookie(cookieName)
-            print("voteDict")
+            print("Anmeldung GET mit vorhandenem Cookie ")
             if clientID in voteDict:
-                print("lecker")
-                return template('index',
-                                name=clientID)
+                print("Cookie in der Datenbank")
+            else:
+                print("Cookie nicht in der Datenbank -> neu hinzugefügt")
+                voteDict[clientID] = VoteEnum.NEUTRAL
+            return template('index', name=clientID)
         else:
             countTinderer = str(len(voteDict) + 1)
             clientID = "TinderGuru " + countTinderer
             response.set_cookie(cookieName, clientID)
             voteDict[clientID] = VoteEnum.NEUTRAL
-            print(voteDict)
+            print("Anmeldung ohne Coockie. Neue Datenbank: " + voteDict)
             return template('index', name=clientID, css = send_static("style.css"), qrc = send_static("qrcode.png"))
     finally:
         pass
 
-@route('/filename:path>')
+@route('./<filename:path>')
 def send_static(filename):
     print("lecker2")
     return static_file(filename, root='views/')       
